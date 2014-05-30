db807cf411466e27328fb30a6a361176281a5780
Nikolai Plath <der.el.kuku@gmail.com>
2014-05-30 05:20:34 -0500

    chg php 10

--- .travis/scripts/apt-get.sh
+++ .travis/scripts/apt-get.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-EXTRA_PACKETS="apache2.2-common apache2-bin apache2-data libapache2-mod-fastcgi php5 php5-mysql"
+EXTRA_PACKETS="apache2 libapache2-mod-fastcgi php5 php5-mysql"
 
 if [ "$1" ]
 then
@@ -10,6 +10,7 @@ fi
 echo "---> Starting $(tput bold ; tput setaf 2)packets installation$(tput sgr0)"
 echo "---> Packets to install : $(tput bold ; tput setaf 3)$EXTRA_PACKETS$(tput sgr0)"
 
-sudo apt-add-repository ppa:ondrej/php5 -y
+sudo apt-add-repository -y ppa:ondrej/apache2
+sudo apt-add-repository -y ppa:ondrej/php5
 sudo apt-get update
 sudo apt-get install -y --force-yes $EXTRA_PACKETS
