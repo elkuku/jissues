fad8e8240f7ed429392868d6c0fe97c10e648065
Nikolai Plath <der.el.kuku@gmail.com>
2014-05-30 04:58:01 -0500

    chg php 3

--- .travis/scripts/apt-get.sh
+++ .travis/scripts/apt-get.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-EXTRA_PACKETS="apache2 libapache2-mod-fastcgi php5-mysql"
+EXTRA_PACKETS="apache2 libapache2-mod-fastcgi php5 php5-mysql"
 if [ "$1" ]
 then
     EXTRA_PACKETS="$EXTRA_PACKETS $1"
@@ -9,10 +9,6 @@ fi
 echo "---> Starting $(tput bold ; tput setaf 2)packets installation$(tput sgr0)"
 echo "---> Packets to install : $(tput bold ; tput setaf 3)$EXTRA_PACKETS$(tput sgr0)"
 
-
+sudo apt-add-repository ppa:ondrej/php5
 sudo apt-get update
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:ondrej/php5
-sudo apt-get update
-sudo apt-get install php5
 sudo apt-get install -y --force-yes $EXTRA_PACKETS
